25ef1f5f3fee074a2dd71a70081e9305bddaa5b7
JakobDeich <104494320+JakobDeich@users.noreply.github.com>
2022-11-09 18:22:00 +0100

    Add files via upload

--- ML.py
+++ ML.py
@@ -17,33 +17,33 @@ import tensorflow as tf
 import logging
 
 
-Test = np.zeros((2,3,4))
+Test = tf.ones((3,4,1))/20
 
-Test_Preds = np.ones((1,3,4))
-
-Test[0]= 0.1
-Test[0][1] = 0.2
-Test[1]= 0.01
+Test_Preds = tf.ones((3,4,1))/5
+Test2 = tf.ones((3,4,1))/10
+# Test[0]= 0.1
+# Test[0][1] = 0.2
+# Test2[1]= 0.01
 # print(Test_Preds)
 Target_Test = 0
-Test2 = tf.ones((3,4,8))
-#point_preds[0]: Galaxy polarisation
-#point_preds[1]: anisotropy correction 
-def cost_func(preds, targets, point_preds):
-    preds = tf.convert_to_tensor(preds)
-    point_preds = tf.convert_to_tensor(point_preds)
-    if tf.keras.backend.ndim(preds) == 3:    
-        cs = tf.keras.backend.mean(point_preds[0] - preds * point_preds[1], axis = 2, keepdims = True)
+Test_fea = tf.ones((3,4,8))/5
+# print(Test2)
+def cost_func(preds, polarisation, anisotropy_corr):
+    # preds = tf.convert_to_tensor(preds)
+    # polarisation = tf.convert_to_tensor(polarisation)
+    # anisotropy_corr = tf.convert_to_tensor(anisotropy_corr)
+    if tf.keras.backend.ndim(preds) == 3:  
+        cs = tf.keras.backend.mean(polarisation - preds * anisotropy_corr, axis = 1, keepdims = True)
         cs_square = tf.keras.backend.square(cs)
-        cs_square_mean = tf.keras.backend.mean(cs_square, keepdims = True) - targets
+        cs_square_mean = tf.keras.backend.mean(cs_square) 
         return cs_square_mean
     else:
         print('false number of dimensions')
 
-# print(cost_func(Test_Preds, Target_Test, Test))
+# print(cost_func(Test_Preds, Test2, Test))
 
 
-nreas = 4
+# nreas = 4
 # input_fea=tf.keras.Input(8,dtype='float32') 
 # input_fea = np.ones((1,8))
 # targets = np.zeros((1,3,4))
@@ -51,41 +51,36 @@ nreas = 4
 # targets = tf.convert_to_tensor(targets)
 # input_pointpreds = Test
 # print(input_fea)
-tf.compat.v1.enable_eager_execution()
 
-def mod(Input, nreas = 4, ncas = 3, nfea = 8, hidden_layers = (3,3)):
-    input_fea=tf.keras.Input((ncas, nreas, nfea),dtype='float32', name="Features") #feat
-    input_tar=tf.keras.Input((1, 1),dtype='float32', name="Targets") #targets
-    inputs= [input_fea,input_tar]
-    input_pointpreds=tf.keras.Input((2, ncas, nreas),dtype='float32', name="Point_preds")
-    inputs.append(input_pointpreds)
+def mod(auxilary_fea1, auxilary_fea2, nreas = 4, nfea = 8, hidden_layers = (3,3)):
+    input_fea=tf.keras.Input((nreas, nfea),dtype='float32', name="Features") #feat
+    # input_tar=tf.keras.Input((1, 1),dtype='float32', name="Targets") #targets
+    # input_fea= [input_fea]
+    # auxilary_fea1=tf.keras.Input((nreas,1),dtype='float32', name="auxilary_features_1")
+    # auxilary_fea2=tf.keras.Input((nreas,1),dtype='float32', name="auxilary_features_2")
     model = tf.keras.Sequential()
-    model.add(tf.keras.Input(shape=(nfea,))) #reas and cases??
+    # model.add(tf.keras.Input(shape=(nreas, nfea))) #reas and cases??
     for hidden_layer in hidden_layers:
-        # model.add(tf.keras.layers.Dense(hidden_layer))
         Layer = layer.TfbilacLayer(hidden_layer)
-        y = Layer(?) #what is the input here?
-        # print(y)
         model.add(Layer)
         model.add(tf.keras.layers.Activation('sigmoid'))
-        print('Hallo')
-    model.add(tf.keras.layers.Dense(1))
-    # model.summary()
-    x = model(inputs[0])
-    loss_func = cost_func(x, inputs[1], inputs[2])
+    model.add(layer.TfbilacLayer(1))
+    x = model(input_fea)
     outputs = [x]
-    model=(tf.keras.Model(inputs=inputs,outputs=outputs))
-    model.add_loss(loss_func)
+    # model.summary()
+    model=(tf.keras.Model(inputs=input_fea,outputs=outputs))
+    model.add_loss(cost_func(x, auxilary_fea1, auxilary_fea2))
+    model.compile(
+        loss = None,
+        optimizer=tf.keras.optimizers.Adam(learning_rate=0.1))
     return model
 
-model = mod(Test2)
-model.compile(
-    loss = None,
-    optimizer=tf.keras.optimizers.Adam(learning_rate=0.1))
-training_data = [Test2, Target_Test, Test]
-# history = model.fit(x = input_fea, epochs = 100, verbose = 0) 
+model = mod(Test2, Test)
+# training_data = [Test2, Target_Test, Test]
+history = model.fit(x = Test_fea, y = None, epochs = 100, verbose = 0) 
 
-# hist = pd.DataFrame(history.history)
+hist = pd.DataFrame(history.history)
+print(hist)
 # hist['epoch'] = history.epoch
 # hist.tail()
 
