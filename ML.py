b4871b335570a3078470f1c90f638716af52f7b7
JakobDeich <104494320+JakobDeich@users.noreply.github.com>
2022-11-14 16:20:41 +0100

    Add files via upload

--- ML.py
+++ ML.py
@@ -12,6 +12,9 @@ from ML_Tensorflow.python.ML_Tensorflow import layer
 from ML_Tensorflow.python.ML_Tensorflow import models
 import config
 from astropy.table import Table
+import graphviz
+import pydot
+from keras.utils.vis_utils import plot_model
 # import ML_Tensorflow.python.ML_Tensorflow
 
 import tensorflow as tf
@@ -23,18 +26,23 @@ table = Table.read(mydir + '/Measured_ksb.fits')
 
 #features: e2 components missing
 #features = np.array([table['aperture_sum'].T,table['sigma_mom'],table['rho4_mom'], table['sigma_mom_psf'], table['e1_cal'], table['e1_cal_psf']] )
-Test = tf.ones((33,10000,1))/20
+Test = np.ones((33,10000,1))/20
 
 Test_Preds = tf.ones((100,10000,1))*1.2
 Test2 = tf.ones((33,10000,1))/10
 
 # ncas = table.meta['N_CAS']
-ncas = 30
+ncas = table.meta['N_CAS']
 nreas = table.meta['N_REA']*table.meta['N_CANC']
 feas = ['aperture_sum','sigma_mom', 'rho4_mom', 'sigma_mom_psf', 'e1_cal', 'e1_cal_psf']
 ac = np.zeros((ncas, nreas,1))
 e1 = np.zeros((ncas, nreas,1))
 features = np.zeros((ncas, nreas,6))
+fea_max = np.zeros(6)
+count = 0
+for i in feas:
+    fea_max[count] = max(abs(table[i]))
+    count = count + 1
 for i in range(ncas):
     path = config.workpath('Test2/Measured_ksb_' + str(i) +'.fits')
     table =Table.read(path)
@@ -43,11 +51,18 @@ for i in range(ncas):
         ac[i][j][0] = table[j]['anisotropy_corr']
         e1[i][j][0] = table[j]['e1_cal']
         for k in feas:
-            features[i][j][count_fea] = table[j][k]
+            features[i][j][count_fea] = table[j][k]/fea_max[count_fea]
             count_fea = count_fea + 1
-ac = tf.convert_to_tensor(np.float32(ac))
-e1 = tf.convert_to_tensor(np.float32(e1))
-features = tf.convert_to_tensor(np.float32(features))
+flags = []
+for i in range(ncas):
+    if (np.mean(features[i,:,0])*fea_max[0] < 500):
+        flags.append(i)
+features = np.delete(features, flags, 0)
+ac = np.delete(ac, flags, 0)
+e1 = np.delete(e1, flags, 0)
+# ac = tf.convert_to_tensor(np.float32(ac))
+# e1 = tf.convert_to_tensor(np.float32(e1))
+# features = tf.convert_to_tensor(np.float32(features))
 # print(len(features[1]))
 # Test[0]= 0.1
 # Test[0][1] = 0.2
@@ -81,12 +96,12 @@ def cost_func(preds, polarisation, anisotropy_corr):
 # input_pointpreds = Test
 # print(input_fea)
 
-def mod(auxilary_fea1, auxilary_fea2, nreas = nreas, nfea = 6, hidden_layers = (3,3)):
+def mod(nreas = nreas, nfea = 6, hidden_layers = (3,3)):
     input_fea=tf.keras.Input((nreas, nfea),dtype='float32', name="Features") #feat
     # input_tar=tf.keras.Input((1, 1),dtype='float32', name="Targets") #targets
     # input_fea= [input_fea]
-    # auxilary_fea1=tf.keras.Input((nreas,1),dtype='float32', name="auxilary_features_1")
-    # auxilary_fea2=tf.keras.Input((nreas,1),dtype='float32', name="auxilary_features_2")
+    auxilary_fea1=tf.keras.Input((nreas,1),dtype='float32', name="auxilary_features_1")
+    auxilary_fea2=tf.keras.Input((nreas,1),dtype='float32', name="auxilary_features_2")
     model = tf.keras.Sequential()
     # model.add(tf.keras.Input(shape=(nreas, nfea))) #reas and cases??
     for hidden_layer in hidden_layers:
@@ -97,15 +112,17 @@ def mod(auxilary_fea1, auxilary_fea2, nreas = nreas, nfea = 6, hidden_layers = (
     x = model(input_fea)
     outputs = [x]
     # model.summary()
-    model=(tf.keras.Model(inputs=input_fea,outputs=outputs))
+    model=(tf.keras.Model(inputs=[input_fea, auxilary_fea1, auxilary_fea2],outputs=outputs))
     model.add_loss(cost_func(x, auxilary_fea1, auxilary_fea2))
-    model.compile(
-        loss = None,
-        optimizer=tf.keras.optimizers.Adam(learning_rate=0.1))
     return model
-model = mod(e1, ac)
-history = model.fit(x = features, y = None, epochs = 100, verbose = 0) 
-print(model.predict(x = features))
+model = mod()
+model.compile(
+        loss = None,
+        optimizer=tf.keras.optimizers.Adam(learning_rate=0.1), metrics = [])
+history = model.fit([features, e1, ac], None, epochs = 100, verbose = 2, batch_size = None) 
+print(model.predict(x = [features, e1, ac]))
+tf.keras.utils.plot_model(model, to_file = 'Plots2/model.png', show_shapes=True)
+
 # hist = pd.DataFrame(history.history)
 # hist['epoch'] = history.epoch
 # hist.tail()
