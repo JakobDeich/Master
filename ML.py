e0cabf5c9cb8373f75b1911cae811397fe8c3341
JakobDeich <104494320+JakobDeich@users.noreply.github.com>
2022-11-16 16:55:31 +0100

    Add files via upload

--- ML.py
+++ ML.py
@@ -5,7 +5,7 @@ import random
 import pickle
 import matplotlib
 import astropy
-matplotlib.use('Agg')
+# matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import pandas as pd
 from ML_Tensorflow.python.ML_Tensorflow import layer
@@ -21,89 +21,98 @@ import tensorflow as tf
     
 import logging
 
-mydir = config.workpath('Test2')
+mydir = config.workpath('Test3')
 table = Table.read(mydir + '/Measured_ksb.fits')
 
-#features: e2 components missing
-#features = np.array([table['aperture_sum'].T,table['sigma_mom'],table['rho4_mom'], table['sigma_mom_psf'], table['e1_cal'], table['e1_cal_psf']] )
-Test = np.ones((33,10000,1))/20
+def threeD_to_oneD(array, ncas, nrea):
+    tab = np.zeros(ncas*nrea)
+    for case in range(ncas):
+        for rea in range(nrea):
+            tab[case * nrea + rea] = array[case][rea][0]       
+    return tab
 
-Test_Preds = tf.ones((100,10000,1))*1.2
-Test2 = tf.ones((33,10000,1))/10
+def oneD_to_threeD(path, parameter, ncas, nreas):
+    array = np.zeros((ncas , nreas,1))
+    for i in range(ncas):
+        path2 = config.workpath(path + '/Measured_ksb_' + str(i) +'.fits')
+        table =Table.read(path2)
+        for j in range(nreas):
+            array[i][j][0] = table[j][parameter]
+    return array
 
-# ncas = table.meta['N_CAS']
-ncas = table.meta['N_CAS']
-nreas = table.meta['N_REA']*table.meta['N_CANC']
-feas = ['aperture_sum','sigma_mom', 'rho4_mom', 'sigma_mom_psf', 'e1_cal', 'e1_cal_psf']
-ac = np.zeros((ncas, nreas,1))
-e1 = np.zeros((ncas, nreas,1))
-features = np.zeros((ncas, nreas,6))
-fea_max = np.zeros(6)
-count = 0
-for i in feas:
-    fea_max[count] = max(abs(table[i]))
-    count = count + 1
-for i in range(ncas):
-    path = config.workpath('Test2/Measured_ksb_' + str(i) +'.fits')
-    table =Table.read(path)
-    for j in range(nreas):
-        count_fea = 0
-        ac[i][j][0] = table[j]['anisotropy_corr']
-        e1[i][j][0] = table[j]['e1_cal']
-        for k in feas:
-            features[i][j][count_fea] = table[j][k]/fea_max[count_fea]
-            count_fea = count_fea + 1
-flags = []
-for i in range(ncas):
-    if (np.mean(features[i,:,0])*fea_max[0] < 500):
-        flags.append(i)
-features = np.delete(features, flags, 0)
-ac = np.delete(ac, flags, 0)
-e1 = np.delete(e1, flags, 0)
-# ac = tf.convert_to_tensor(np.float32(ac))
-# e1 = tf.convert_to_tensor(np.float32(e1))
-# features = tf.convert_to_tensor(np.float32(features))
-# print(len(features[1]))
-# Test[0]= 0.1
-# Test[0][1] = 0.2
-# Test2[1]= 0.01
-# print(Test_Preds)
-# Target_Test = 0
-Test_fea = tf.ones((33,10000,6))/5
-# print(Test2)
+def Data_processing(path, case_percentage = 1):
+    mydir = config.workpath(path)
+    table = Table.read(mydir + '/Measured_ksb.fits')
+    ncas = table.meta['N_CAS']
+    Training = np.arange(round(ncas * case_percentage))
+    Testing = np.arange(round(ncas * case_percentage), ncas)
+    nreas = table.meta['N_REA']*table.meta['N_CANC']
+    feas = ['aperture_sum','sigma_mom', 'rho4_mom', 'sigma_mom_psf', 'e1_cal', 'e1_cal_psf']
+    ac = np.zeros((round(ncas * case_percentage), nreas,1))
+    e1 = np.zeros((round(ncas * case_percentage), nreas,1))
+    Pg = np.zeros((round(ncas * case_percentage), nreas,1))
+    ac_test = np.zeros((ncas - round(ncas * case_percentage), nreas,1))
+    e1_test = np.zeros((ncas - round(ncas * case_percentage), nreas,1))
+    Pg_test = np.zeros((ncas -round(ncas * case_percentage), nreas,1))
+    features_test = np.zeros((ncas -round(ncas * case_percentage), nreas,6))
+    features = np.zeros((round(ncas * case_percentage), nreas,6))
+    fea_max = np.zeros(6)
+    count = 0
+    for i in feas:
+        fea_max[count] = max(abs(table[i]))
+        count = count + 1
+    for i in Training:
+        path2 = config.workpath(path + '/Measured_ksb_' + str(i) +'.fits')
+        table =Table.read(path2)
+        for j in range(nreas):
+            count_fea = 0
+            ac[i][j][0] = table[j]['anisotropy_corr']
+            e1[i][j][0] = table[j]['e1_cal']
+            Pg[i][j][0] = table[j]['Pg_11']
+            for k in feas:
+                features[i][j][count_fea] = table[j][k]/fea_max[count_fea]
+                count_fea = count_fea + 1
+    for i,m in enumerate(Testing):
+        path2 = config.workpath(path + '/Measured_ksb_' + str(m) +'.fits')
+        table =Table.read(path2)
+        for j in range(nreas):
+            ac_test[i][j][0] = table[j]['anisotropy_corr']
+            e1_test[i][j][0] = table[j]['e1_cal']
+            Pg_test[i][j][0] = table[j]['Pg_11']
+            for k,l in enumerate(feas):
+                features_test[i][j][k] = table[j][l]/fea_max[k]
+    flags = []
+    for i in range(round(ncas * case_percentage)):
+        if (np.mean(features[i,:,0])*fea_max[0] < 0):
+            flags.append(i)
+    features = np.delete(features, flags, 0)
+    ac = np.delete(ac, flags, 0)
+    e1 = np.delete(e1, flags, 0)
+    flags_test = []
+    for i in range(ncas - round(ncas * case_percentage)):
+        if (np.mean(features_test[i,:,0])*fea_max[0] < 0):
+            flags_test.append(i)
+    features_test = np.delete(features_test, flags, 0)
+    ac_test = np.delete(ac_test, flags, 0)
+    e1_test = np.delete(e1_test, flags, 0)
+    return features, ac, e1, Pg, nreas, len(feas), features_test, ac_test, e1_test, Pg_test
+
+
 def cost_func(preds, polarisation, anisotropy_corr):
-    # preds = tf.convert_to_tensor(preds)
-    # polarisation = tf.convert_to_tensor(polarisation)
-    # anisotropy_corr = tf.convert_to_tensor(anisotropy_corr)
     if tf.keras.backend.ndim(preds) == 3:  
         cs = tf.keras.backend.mean(polarisation - preds * anisotropy_corr, axis = 1, keepdims = True)
-        print(cs)
         cs_square = tf.keras.backend.square(cs)
         cs_square_mean = tf.keras.backend.mean(cs_square, keepdims = True) 
         return cs_square_mean
     else:
         print('false number of dimensions')
 
-# print(cost_func(Test_Preds, e1, ac))
 
-
-# nreas = 4
-# input_fea=tf.keras.Input(8,dtype='float32') 
-# input_fea = np.ones((1,8))
-# targets = np.zeros((1,3,4))
-# input_fea = tf.convert_to_tensor(input_fea)
-# targets = tf.convert_to_tensor(targets)
-# input_pointpreds = Test
-# print(input_fea)
-
-def mod(nreas = nreas, nfea = 6, hidden_layers = (3,3)):
+def mod(nreas, nfea, hidden_layers = (3,3)):
     input_fea=tf.keras.Input((nreas, nfea),dtype='float32', name="Features") #feat
-    # input_tar=tf.keras.Input((1, 1),dtype='float32', name="Targets") #targets
-    # input_fea= [input_fea]
-    auxilary_fea1=tf.keras.Input((nreas,1),dtype='float32', name="auxilary_features_1")
-    auxilary_fea2=tf.keras.Input((nreas,1),dtype='float32', name="auxilary_features_2")
+    auxilary_fea1=tf.keras.Input((nreas,1),dtype='float32', name="auxilary_features_1") #polarisation
+    auxilary_fea2=tf.keras.Input((nreas,1),dtype='float32', name="auxilary_features_2") #anisotropy correction
     model = tf.keras.Sequential()
-    # model.add(tf.keras.Input(shape=(nreas, nfea))) #reas and cases??
     for hidden_layer in hidden_layers:
         Layer = layer.TfbilacLayer(hidden_layer)
         model.add(Layer)
@@ -111,17 +120,57 @@ def mod(nreas = nreas, nfea = 6, hidden_layers = (3,3)):
     model.add(layer.TfbilacLayer(1))
     x = model(input_fea)
     outputs = [x]
-    # model.summary()
     model=(tf.keras.Model(inputs=[input_fea, auxilary_fea1, auxilary_fea2],outputs=outputs))
     model.add_loss(cost_func(x, auxilary_fea1, auxilary_fea2))
     return model
-model = mod()
+
+
+features, ac, e1,Pg, nreas, nfea, features_test, ac_test, e1_test, Pg_test = Data_processing('Test3', 1)
+model = mod(nreas, nfea)
 model.compile(
-        loss = None,
-        optimizer=tf.keras.optimizers.Adam(learning_rate=0.1), metrics = [])
-history = model.fit([features, e1, ac], None, epochs = 100, verbose = 2, batch_size = None) 
-print(model.predict(x = [features, e1, ac]))
-tf.keras.utils.plot_model(model, to_file = 'Plots2/model.png', show_shapes=True)
+    loss = None,
+    optimizer=tf.keras.optimizers.Adam(learning_rate=0.1),
+    metrics = [])
+history = model.fit(
+    [features, e1, ac], 
+    None, 
+    epochs = 100,
+    verbose = 0, 
+    batch_size = None) 
+bsm = model.predict(x = [features, e1, ac])
+# print(bsm)
+# e = np.mean((e1 -  bsm * ac)**2)
+# e2 = np.mean((e1 -  ac)**2)
+# P_g = np.mean(Pg)
+aper_sum = oneD_to_threeD('Test3', 'sigma_mom', 75, nreas)
+bsm_mean = np.zeros((75,1))
+aper_mean = np.zeros((75,1))    
+points = 5
+for i in range(75):
+    bsm_mean[i] = np.mean(bsm[i,:])
+    aper_mean[i] = np.mean(aper_sum[i,:])
+list1 = np.linspace(min(aper_mean),max(aper_mean), points, endpoint = False)
+bsms = []
+step = max(aper_mean)/points
+for i in range(points):
+    Good1 = aper_mean > list1[i]
+    Good2 = aper_mean < list1[i] + step
+    Good = np.logical_and(Good1, Good2)
+    bsms.append(np.mean(bsm_mean[Good]))
+plt.scatter(list1,bsms)
+plt.xlabel('sigma moments moments')
+plt.ylabel('boost factor')
+plt.savefig('Plots2/sigma_boost.pdf')
+    
+# print(e - e2)
+# print(model.evaluate(x = [features, e1, ac], y = None))
+# print(bsm)
+# plt.plot(range(100), (history.history['loss']), label= str(i * 100) +' percent of cases')
+# plt.xlabel('epochs')
+# plt.ylabel('loss')
+# print(tf.keras.backend.ndim(bsm))
+# print(cost_func(bsm, e1, ac))
+# tf.keras.utils.plot_model(model, to_file = 'Plots2/model.png', show_shapes=True)
 
 # hist = pd.DataFrame(history.history)
 # hist['epoch'] = history.epoch
